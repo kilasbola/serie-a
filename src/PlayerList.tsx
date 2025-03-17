3348376a1948b65c460c318c10fea519b1528d33
sadapphone <sandyartha@hotmail.com>
2025-03-18 06:56:24 +0700

    Como 1907

--- src/PlayerList.tsx
+++ src/PlayerList.tsx
@@ -10,7 +10,7 @@ import {
 } from "remotion";
 import React, { useMemo, useEffect, useState } from "react";
 import { loadFont as loadRubik } from "@remotion/google-fonts/Rubik";
-import rawTopPlayers from "../public/data/cagliari_calcio_top_goals.json";
+import rawTopPlayers from "../public/data/como_1907_top_goals.json";
 import { TopPlayer, validateTopPlayers, } from "./types/schema";
 import { PlayerCard } from "./components/PlayerCardv1";
 import { getLogoCode } from "./utils/getLogoClub";
@@ -101,7 +101,7 @@ const IntroTitle: React.FC<{ person?: TopPlayer }> = ({ person }) => {
           transform: `translateY(${titleSlideUp}%)`,
         }}
       >
-        Cagliari Calcio All-Time Top Scorers
+        Como 1907 All-Time Top Scorers
       </h1>
 
       <h2
