6c35a8c3b34169b544cde14ae2cdb9197d7c59c6
ass <jurus32@gmail.com>
2025-03-21 19:29:08 +0700

    Hellas Verona

--- src/Root.tsx
+++ src/Root.tsx
@@ -8,7 +8,7 @@ export const RemotionRoot: React.FC = () => {
       <Composition
         id="PlayerListCard"
         component={PlayerList}
-        durationInFrames={60*10} // default 60*200
+        durationInFrames={60*200} // default 60*200 or 60*10
         fps={60}
         width={2560}
         height={1440}
