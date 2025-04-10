28ab6aeb4acc22813f658cdd3306dcd85305e724
ass <jurus32@gmail.com>
2025-04-10 19:08:27 +0700

    Rans Nusantara

--- src/PlayerList.tsx
+++ src/PlayerList.tsx
@@ -41,8 +41,8 @@ import { getLogoCode } from "./utils/getLogoClub";
 // import rawTopPlayers from "../public/id/psm_makassar_top_goals.json";
 // import rawTopPlayers from "../public/id/psps_pekanbaru_top_goals.json";
 // import rawTopPlayers from "../public/id/pss_sleman_top_goals.json";
-import rawTopPlayers from "../public/id/ps_barito_putera_top_goals.json";
-// import rawTopPlayers from "../public/id/rans_nusantara_fc_top_goals.json";
+// import rawTopPlayers from "../public/id/ps_barito_putera_top_goals.json";
+import rawTopPlayers from "../public/id/rans_nusantara_fc_top_goals.json";
 // import rawTopPlayers from "../public/id/semen_padang_fc_top_goals.json";
 // import rawTopPlayers from "../public/id/sriwijaya_fc_top_goals.json";
 
